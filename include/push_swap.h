a0204d6840b9c457370694d509c53d16cdbad396
Albina Peposhi <apeposhi@3-B-8.42heilbronn.de>
2023-03-17 18:50:06 +0100

    added changes

---
+++ include/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 11:31:45 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/03/17 17:46:42 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/includes/libft.h"
+
+typedef struct s_list {
+	int value;
+	struct s_list* next;
+}				t_list;
+
---
+++ main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 11:29:42 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/03/16 12:31:16 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+Get the freshest e-commerce trends of 2023 during the new Commerce Pulse event. Discover how consumers are shopping this year, what are the latest regional and global trends, and look into metrics and analytics from over 500 retailers. More info and registration here: https://lnkd.in/dHhrntwE 
+#ecommerce #commerce #digitalmarketing
