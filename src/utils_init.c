fd8a94ee262dde1a9a02b2d28632ae59f0d99672
Albina Peposhi <apeposhi@3-B-9.42heilbronn.de>
2023-04-11 12:16:21 +0200

    changes

--- src/utils_init.c
+++ src/utils_init.c
@@ -6,16 +6,29 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:22:50 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/09 14:51:10 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/11 12:05:16 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	initialize_stacks(t_struct **a, t_struct **b)
+static void initialize_stack(t_struct **a, int argc, char **argv)
 {
-    a = (t_struct **)malloc(sizeof(t_struct));
-	b = (t_struct **)malloc(sizeof(t_struct));
-    *a = NULL;
-    *b = NULL;
+    t_struct    *temp;
+    char        **arguments;
+    int         index;
+
+    index = -1;
+    if (argc == 2)
+        arguments = ft_split(argv[1], ' ');
+    else
+    {
+        index = 0;
+        arguments = argv;
+    }
+    while (arguments[++index])
+    {
+        temp = ft_lstnew(ft_atoi(arguments[index]));
+        ft_lstadd_back(a, temp);;
+    }
 }
