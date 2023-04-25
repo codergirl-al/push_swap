84885292ec2b363c60afa83ac103519310a1e8df
Albina Peposhi <codegirl.al@gmail.com>
2023-04-26 01:01:37 +0200

    Added algorithm sort functionality

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 13:09:14 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/25 23:02:06 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/26 00:31:09 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ static int	ft_sort_control(t_struct **a, t_struct **b)
 		ft_free_stack(b);
 		return (0);
 	}
+	if (ft_stack_size(a) <= 5)
+		//sorting_alg_simple
+	// else
+		//sorting_alg_RADIX
 	ft_sort_control(a, b);
 	return (0);
 }
