d6d93a6febdf91c873d981603fa2b2b47238f286
Albina Peposhi <codegirl.al@gmail.com>
2023-04-25 18:57:14 +0200

    Added indexes for stack elements after init

--- src/utils_init.c
+++ src/utils_init.c
@@ -6,11 +6,39 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:22:50 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/12 13:42:57 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:39:30 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+t_struct *ft_get_min_value(t_struct **s)
+{
+    t_struct    *head;
+    t_struct    *min;
+    
+    head = *s;
+    while (head != NULL)
+    {
+        if (head->index == -1 && (min == NULL || head->value < min->value))
+            min = head;
+        head = head->next;
+    }
+    return min;
+}
+
+void ft_give_indexes_to_stack(t_struct **s)
+{
+    t_struct    *head;
+    int         index;
+
+    index = 0;
+    while (ft_get_min_value(s) != NULL)
+    {
+        head = ft_get_min_value(s);
+        head->index = ++index;
+    }
+}
 
 void init_stack(t_struct **a, int argc, char **argv)
 {
@@ -31,4 +59,7 @@ void init_stack(t_struct **a, int argc, char **argv)
         temp = ft_struct_new(ft_atoi(arguments[index]));
         ft_struct_add_back(a, temp);
     }
+    ft_give_indexes_to_stack(a);
+    if (argc == 2)
+        ft_free_arguments(arguments);
 }
