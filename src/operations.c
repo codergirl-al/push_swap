8271ad72dba486e12e4f4adc679ccbe24d24949d
Albina Peposhi <codegirl.al@gmail.com>
2023-04-29 16:53:29 +0200

    Operations add output function

--- src/operations.c
+++ src/operations.c
@@ -6,19 +6,19 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 13:16:47 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/05 17:55:15 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/27 22:57:25 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void swap(t_struct **stack)
+int swap(t_struct **stack, char *s)
 {
 	t_struct	*temp;
 	t_struct	*second;
 
 	if (*stack == NULL || (*stack)->next == NULL)
-		return ;
+		return (0);
 	
 	temp = *stack;
 	second = (*stack)->next;
@@ -28,64 +28,58 @@ void swap(t_struct **stack)
 	temp->next = second->next;
 	// The new second node points to the next node
 	second->next = temp;
+    ft_print_operation("s", s);
+    return (0);
 }
 
-void	double_swap(t_struct **a, t_struct **b)
+int	double_swap(t_struct **a, t_struct **b)
 {
-	swap(a);
-	swap(b);
+	swap(a, "x");
+	swap(b, "x");
+	ft_print_operation("s", "s");
+	return (0);
 }
 
-void	push(t_struct **src, t_struct **dest)
+int	push(t_struct **src, t_struct **dest, char *s)
 {
 	t_struct	*temp;
 	t_struct	*new_src_top;
 	
 	if (*src == NULL)
-		return ;
+		return (0);
 	temp = *src;
 	new_src_top = (*src)->next;
 	temp->next = *dest;
 	*dest = temp;
 	*src = new_src_top;
+	if (s[0] != 'x')
+		ft_print_operation("p", s);
+	return (0);
 }
 
-void	rotate(t_struct **stack)
+int	rotate(t_struct **stack, char *s)
 {
 	t_struct	*temp;
 	t_struct	*new_first;
 	t_struct	*last;
 	
 	if (*stack == NULL)
-		return ;
+		return (0);
 	temp = *stack;
 	new_first = (*stack)->next;
 	last = 
 	*stack = new_first;
 	temp->next = NULL;
 	last->next = temp;
+    if (s[0] != 'x')
+		ft_print_operation("r", s);
+	return (0);
 }
 
-void	double_rotate(t_struct **a, t_struct **b)
+int	double_rotate(t_struct **a, t_struct **b)
 {
-	rotate(a);
-	rotate(b);
-}
-
-void	reverse_rotate(t_struct **stack)
-{
-	t_struct	*last;
-	t_struct	*second_last;
-
-	if (*stack == NULL || (*stack)->next == NULL)
-		return ;
-	last = *stack;
-	while (last->next != NULL)
-		last = last->next;
-	second_last = *stack;
-	while (second_last->next != last)
-		second_last = second_last->next;
-	last->next = *stack;
-	*stack = last;
-	second_last->next = NULL;
+	rotate(a, "x");
+	rotate(b, "x");
+	ft_print_operation("rr", "r");
+	return (0);
 }
