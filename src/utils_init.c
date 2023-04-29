29fee1f851974315625b5989345e79adf58d1356
Albina Peposhi <codegirl.al@gmail.com>
2023-04-29 16:52:36 +0200

    Init debugging gone right

--- src/utils_init.c
+++ src/utils_init.c
@@ -6,26 +6,32 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:22:50 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/25 18:39:30 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/29 16:51:03 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_struct *ft_get_min_value(t_struct **s)
-{
-    t_struct    *head;
-    t_struct    *min;
-    
-    head = *s;
-    while (head != NULL)
-    {
-        if (head->index == -1 && (min == NULL || head->value < min->value))
-            min = head;
-        head = head->next;
+t_struct *ft_get_min_value(t_struct **s) {
+  t_struct *head;
+  t_struct *min;
+  int has_min;
+
+  min = NULL;
+  has_min = 0;
+  head = *s;
+  if (head) {
+    while (head) {
+      if ((head->index == -1) && (!has_min || head->value < min->value)) {
+        min = head;
+        has_min = 1;
+      }
+      head = head->next;
     }
-    return min;
+  }
+  return (min);
 }
+
 
 void ft_give_indexes_to_stack(t_struct **s)
 {
@@ -36,30 +42,31 @@ void ft_give_indexes_to_stack(t_struct **s)
     while (ft_get_min_value(s) != NULL)
     {
         head = ft_get_min_value(s);
-        head->index = ++index;
+        head->index = index;
+        index++;
     }
 }
 
 void init_stack(t_struct **a, int argc, char **argv)
 {
-    t_struct    *temp;
-    char        **arguments;
-    int         index;
+    t_struct *new;
+    char **args;
+    int i;
 
-    index = -1;
+    i = 0;
     if (argc == 2)
-        arguments = ft_split(argv[1], ' ');
-    else
-    {
-        index = 0;
-        arguments = argv;
+        args = ft_split(argv[1], ' ');
+    else {
+        i = 1;
+        args = argv;
     }
-    while (arguments[++index])
+    while (args[i])
     {
-        temp = ft_struct_new(ft_atoi(arguments[index]));
-        ft_struct_add_back(a, temp);
+        new = ft_struct_new(ft_atoi(args[i]));
+        ft_struct_add_back(a, new);
+        i++;
     }
     ft_give_indexes_to_stack(a);
     if (argc == 2)
-        ft_free_arguments(arguments);
+        ft_free_arguments(args);
 }
