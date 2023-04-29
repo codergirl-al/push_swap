d2a7bb872a91a3a2fadcfb92b669b2e80720f120
Albina Peposhi <codegirl.al@gmail.com>
2023-04-29 16:53:46 +0200

    DEBUGGING.

--- src/main.c
+++ src/main.c
@@ -6,44 +6,42 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 13:09:14 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/26 17:39:46 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/29 16:50:44 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-static int	ft_sort_control(t_struct **a, t_struct **b, char **str)
+static void	ft_sort_control(t_struct **a, t_struct **b)
 {
-	if (ft_is_sorted(a))
-	{
-		ft_free_stack(a);
-		ft_free_stack(b);
-		return (0);
-	}
 	if (ft_stack_size(*a) <= 5)
 		simple_sort(a, b);
 	else
-		// three_chunk_sort(a, b);
-		ft_printf("hloow");
-	ft_sort_control(a, b, str);
-	return (1);
+		radix_sort(a, b);
 }
 
 int	main(int argc, char **argv)
 {
 	t_struct	**a;
 	t_struct	**b;
-	char		*str;
 
 	if (argc < 2)
 		return (-1);
 	validate_args(argc, argv);
 	a = (t_struct **)malloc(sizeof(t_struct));
 	b = (t_struct **)malloc(sizeof(t_struct));
-    *a = NULL;
-    *b = NULL;
+	*a = NULL;
+	*b = NULL;
 	init_stack(a, argc, argv);
-	str = NULL;
-	ft_sort_control(a, b, &str);
+	if (ft_is_sorted(a))
+	{
+		ft_free_stack(a);
+		ft_free_stack(b);
+		return (0);
+	}
+	ft_sort_control(a, b);
+	ft_printf("We're done here\n");
+	ft_free_stack(a);
+	ft_free_stack(b);
 	return (0);
 }
