890427ef7127fdc0ab91c4e21035ef4737031829
Albina Peposhi <codegirl.al@gmail.com>
2023-04-30 12:17:53 +0200

    norminette cleanup

--- src/utils_list_functions.c
+++ src/utils_list_functions.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_list_functions.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 12:16:25 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/04/30 12:17:39 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/push_swap.h"
 
 t_struct	*ft_struct_new(int value)
@@ -44,15 +56,15 @@ t_struct	*ft_struct_last(t_struct *str)
 
 int	ft_stack_size(t_struct *stack)
 {
-    int len;
+	int	len;
 
-    len = 1;
+	len = 1;
 	if (!stack)
 		return (0);
-    while (stack->next != NULL)
+	while (stack->next != NULL)
 	{
 		len++;
-        stack = stack->next;
-    }
-    return (len);
+		stack = stack->next;
+	}
+	return (len);
 }
